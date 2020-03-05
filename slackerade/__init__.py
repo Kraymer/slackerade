98eb8c80d11e7a8dfde74ca02941ea5e60cff0e2
kraymer <kraymer@gmail.Com>
2020-03-05 22:21:58 +0100

    add setup.py

--- slackerade/__init__.py
+++ slackerade/__init__.py
@@ -5,10 +5,6 @@ import click
 import json
 import requests
 
-
-__VERSION__ = u"0.0.1"
-
-
 @click.command(
     context_settings=dict(help_option_names=["-h", "--help"]),
     help=(
@@ -22,7 +18,7 @@ __VERSION__ = u"0.0.1"
 @click.argument("message", type=str, metavar="MESSAGE", nargs=1)
 @click.argument("emoji", type=str, metavar="EMOJI", nargs=1)
 @click.version_option(__VERSION__)
-def slackerade(channel, username, message, emoji):
+def slackerade_cli(channel, username, message, emoji):
     payload = {"urls": (channel, ),
         "username": username,
         "text": message,
@@ -30,4 +26,4 @@ def slackerade(channel, username, message, emoji):
     requests.post(channel, {"payload": json.dumps(payload)}, verify=False)
 
 if __name__ == "__main__":
-    slackerade()
+    slackerade_cli()
