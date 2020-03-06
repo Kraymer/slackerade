f0121dc2dd1181629696b6b93d2d3fb2db6ad435
kraymer <kraymer@gmail.Com>
2020-03-06 22:20:41 +0100

    blacken the code

--- setup.py
+++ setup.py
@@ -13,32 +13,27 @@ with open(os.path.join(PKG_NAME, "VERSION")) as _file:
     VERSION = _file.read().strip()
 
 # Deploy: python3 setup.py sdist bdist_wheel; twine upload --verbose dist/*
-setup(name=PKG_NAME,
-    version=VERSION if not VERSION.endswith('dev') else '%s%s' % (
-        VERSION, int(time.time())),
-    description='Masquerade yourself as a fictitious user on slack',
-    long_description=open(os.path.join(DIRPATH, 'README.rst')).read(),
-    author='Fabrice Laporte',
-    author_email='kraymer@gmail.com',
-    url='https://github.com/KraYmer/slackerade',
-    license='MIT',
-    platforms='ALL',
-
-    packages=[
-      'slackerade',
-    ],
-      entry_points={
-          'console_scripts': [
-              'slackerade = slackerade:slackerade_cli',
-          ],
-    },
+setup(
+    name=PKG_NAME,
+    version=VERSION
+    if not VERSION.endswith("dev")
+    else "%s%s" % (VERSION, int(time.time())),
+    description="Masquerade yourself as a fictitious user on slack",
+    long_description=open(os.path.join(DIRPATH, "README.rst")).read(),
+    author="Fabrice Laporte",
+    author_email="kraymer@gmail.com",
+    url="https://github.com/KraYmer/slackerade",
+    license="MIT",
+    platforms="ALL",
+    packages=["slackerade",],
+    entry_points={"console_scripts": ["slackerade = slackerade:slackerade_cli",],},
     include_package_data=True,
-    install_requires=open(os.path.join(DIRPATH, 'requirements.txt')).read().split('\n'),
+    install_requires=open(os.path.join(DIRPATH, "requirements.txt")).read().split("\n"),
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Topic :: Communications :: Chat',
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: Communications :: Chat",
     ],
     keywords="slack",
-    python_requires='>=3.5.0',
-    )
+    python_requires=">=3.5.0",
+)
